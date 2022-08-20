d6dae5bb69280d3f48f52274ffbb99cc307c204f
changpanwen <1319997407@qq.com>
2022-08-20 17:49:49 +0800

    测试

---
+++ project.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	///*char a[] = {"Hello World!"};
+	//char *b = a;
+	//int i,t;
+	//t = strlen(a);
+	//for (i = 0; i < t; i++)
+	//	printf("%c",*(b+i));
+	//printf("\n");*/
+	//char a[] = { "Hello World!" };
+	//char *b = a;
+	//while (*b != '\0')
+	//{
+	//	printf("%c",*b++);
+	//}
+	return 0;
+}
